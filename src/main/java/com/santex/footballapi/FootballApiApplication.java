9b932a0b7fca4d5de1e1c9a0a78d02ea0370b6fe
jiclotus <joseignaciocastelli92@gmail.com>
2020-02-17 19:02:34 -0300

    Adding first swagger configuration.

--- src/main/java/com/santex/footballapi/FootballApiApplication.java
+++ src/main/java/com/santex/footballapi/FootballApiApplication.java
@@ -3,6 +3,7 @@ package com.santex.footballapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FootballApiApplication {
 
